8d1bb6d6eb9f739293f82be22cd914ee53cefe7b
Lex de Kort <lexdekort@live.nl>
2022-05-18 23:46:45 +0200

    Add unit tests for encrypting passwords

---
+++ duelsys/TournamentManager/UnitTests/TestEncryptor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BLL.Encryption;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestEncryptor
+    {
+        [TestMethod]
+        public void TestHash()
+        {
+            Encryptor encryptor = new Encryptor();
+            string password = "Hello World";
+
+            SaltKey hashed = encryptor.Hash(password);
+
+            Assert.IsNotNull(hashed);
+        }
+
+        [TestMethod]
+        public void TestVerifyWithCorrectPassword()
+        {
+            Encryptor encryptor = new Encryptor();
+            string password = "Hello World!";
+
+            SaltKey hashed = encryptor.Hash(password);
+            bool result = encryptor.Verify(password, hashed.Key, hashed.Salt);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestVerifyWithIncorrectPassword()
+        {
+            Encryptor encryptor = new Encryptor();
+            string password = "Hello World!";
+            string differentPassword = "Also Hello!";
+
+            SaltKey hashed = encryptor.Hash(password);
+            bool result = encryptor.Verify(differentPassword, hashed.Key, hashed.Salt);
+
+            Assert.IsFalse(result);
+        }
+    }
+}

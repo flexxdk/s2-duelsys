846b4c25fdb0dd805534d79258babe8e17b8a392
Lex de Kort <lexdekort@live.nl>
2022-06-09 21:38:11 +0200

    Add extra unit tests for tournament object

--- duelsys/TournamentManager/UnitTests/TournamentTest.cs
+++ duelsys/TournamentManager/UnitTests/TournamentTest.cs
@@ -1,12 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL.Objects;
+using BLL.Objects.Sports;
+using BLL.Enums;
+using System;
 
 namespace UnitTests
 {
+    [TestClass]
     public class TournamentTest
     {
+        [TestMethod]
+        public void TestTournamentCanStartTrue()
+        {
+            Tournament tournament = new Tournament()
+            {
+                Title = "Average-Minton",
+                Description = "It's also badminton!",
+                Sport = new Badminton(),
+                City = "Eindhoven",
+                Address = "Eindhovense Straat 1",
+                MinContestants = 8,
+                MaxContestants = 14,
+                StartDate = DateTime.Now.Date,
+                EndDate = DateTime.Now.AddDays(7).Date,
+                Status = TournamentStatus.Planned,
+                System = TournamentSystem.RoundRobin
+            };
+
+            bool result = tournament.CanStart(10);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestTournamentCanStartInvalidDate()
+        {
+            Tournament tournament = new Tournament()
+            {
+                Title = "Average-Minton",
+                Description = "It's also badminton!",
+                Sport = new Badminton(),
+                City = "Eindhoven",
+                Address = "Eindhovense Straat 1",
+                MinContestants = 8,
+                MaxContestants = 14,
+                StartDate = DateTime.Now.Date.AddDays(10),
+                EndDate = DateTime.Now.AddDays(15).Date,
+                Status = TournamentStatus.Planned,
+                System = TournamentSystem.RoundRobin
+            };
+
+            bool result = tournament.CanStart(10);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestTournamentCanStartNotEnoughContestants()
+        {
+            Tournament tournament = new Tournament()
+            {
+                Title = "Average-Minton",
+                Description = "It's also badminton!",
+                Sport = new Badminton(),
+                City = "Eindhoven",
+                Address = "Eindhovense Straat 1",
+                MinContestants = 8,
+                MaxContestants = 14,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(7).Date,
+                Status = TournamentStatus.Planned,
+                System = TournamentSystem.RoundRobin
+            };
+
+            bool result = tournament.CanStart(2);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestTournamentCanStartBothStartConditionsFail()
+        {
+            Tournament tournament = new Tournament()
+            {
+                Title = "Average-Minton",
+                Description = "It's also badminton!",
+                Sport = new Badminton(),
+                City = "Eindhoven",
+                Address = "Eindhovense Straat 1",
+                MinContestants = 8,
+                MaxContestants = 14,
+                StartDate = DateTime.Now.Date.AddDays(10),
+                EndDate = DateTime.Now.AddDays(15).Date,
+                Status = TournamentStatus.Planned,
+                System = TournamentSystem.RoundRobin
+            };
+
+            bool result = tournament.CanStart(2);
+
+            Assert.IsFalse(result);
+        }
     }
 }
